a021e8a6efa6e405e65ca8a53204ea92e1ea6063
Bauto2005 <bautodinamo@gmail.com>
2025-10-08 00:04:02 -0300

    Cree los botones, e implemente la funcion salir del juego

---
+++ Rootbound/Assets/Materiales-del-MENU/Menu-Scrips/FuncionSalir.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FuncionSalir : MonoBehaviour
+{
+    public void SalirDelJuego()
+    {
+        Debug.Log("Salir del juego");
+        Application.Quit();
+    }
+
+
+
+}
